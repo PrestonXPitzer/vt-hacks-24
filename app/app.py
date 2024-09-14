917e3e7f5764a53132cd51f84a875dd7477fad47
PrestonXPitzer <preston.pitzer@gmail.com>
2024-09-14 14:59:23 -0400

    added enpoints for the various utilities

--- app/app.py
+++ app/app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, Response, jsonify
 from controllers_yolo import generate_frames, get_pedestrian_count  # Import the controllers module
 
+dataPts = []
+hourlyPts = []
+dailyPts = []
+weeklyPts = []
+
+trueIfIncreasing = False
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,7 +22,43 @@ def video_feed():
 @app.route('/line-length')
 def line_length():
     count = get_pedestrian_count()
+    #increasing decresasin logic
+    if 
+    if len(dataPts) < 60:
+        dataPts.append(count)
+    else:
+        if len(hourlyPts) < 24:
+            hourlyPts.append(sum(dataPts)/len(dataPts))
+        else:
+            if len(dailyPts) < 7:
+                dailyPts.append(sum(hourlyPts)/len(hourlyPts))
+            else:
+                weeklyPts.append(sum(dailyPts)/len(dailyPts))
+                dailyPts = []
+            hourlyPts = []
+        dataPts = []
     return jsonify(lineLength=count)
 
+@app.route('/hourly-average')
+def hourly_average():
+    avg = sum(hourlyPts) / len(hourlyPts) if hourlyPts else 0
+    return jsonify(hourlyAverage=avg)
+
+@app.route('/daily-average')
+def daily_average():
+    avg = sum(dailyPts) / len(dailyPts) if dailyPts else 0
+    return jsonify(dailyAverage=avg)
+
+@app.route('/weekly-average')
+def weekly_average():
+    avg = sum(weeklyPts) / len(weeklyPts) if weeklyPts else 0
+    return jsonify(weeklyAverage=avg)
+
+@app.route('/is-increasing')
+def is_increasing():
+    return jsonify(isIncreasing=trueIfIncreasing)
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0,0,0', port=5000)
