370dc7d0c73c4798639d662351457d7977bb044f
PrestonXPitzer <preston.pitzer@gmail.com>
2024-09-14 16:33:50 -0400

    changed the default render point to be the index file

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('beta.html')
+    return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
