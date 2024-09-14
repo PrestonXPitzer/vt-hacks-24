8a190a598dabb4d67d1027c238448e7cc2aee698
PrestonXPitzer <preston.pitzer@gmail.com>
2024-09-13 22:34:32 -0400

    Added bounding box to the camera imagaes

--- app/controllers.py
+++ app/controllers.py
@@ -1,6 +1,5 @@
 import cv2
 
-# Initialize the video capture object
 cap = cv2.VideoCapture(0)
 
 # Load the Haar cascade
@@ -17,22 +16,27 @@ def get_pedestrian_count():
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     pedestrians = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     
-    count = len(pedestrians) #This can be replaced with a region of interest check at some point, but for now
-    #just return the number of pedestrians detected
+    count = len(pedestrians)  # This can be replaced with a region of interest check at some point, but for now
+    # just return the number of pedestrians detected
 
     return count
 
 def generate_frames():
     while True:
-        # Capture frame-by-frame
-        success, frame = cap.read()
-        if not success:
+        ret, frame = cap.read()
+        if not ret:
             break
-        else:
-            # Encode the frame in JPEG format
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
 
-            # Yield the frame in byte format
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        pedestrians = cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        
+        # Draw bounding boxes
+        for (x, y, w, h) in pedestrians:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+        # Encode the frame in JPEG format
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
